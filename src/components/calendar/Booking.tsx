7acef67cbf4ac86e06d9aec32858648c838b2f6f
segundo <segujuan27@hotmail.com>
2024-02-14 16:46:23 +0100

    fix booking override zindex

--- src/components/calendar/Booking.tsx
+++ src/components/calendar/Booking.tsx
@@ -28,14 +28,14 @@ const Booking: React.FC<IBookingProps> = ({
         draggable
         onDragStart={(e) => onDragStart(e, booking.id, isPickup)}
         onClick={showModal}
-        className="cursor-pointer bg-white sm:rounded-lg shadow-lg relative mb-4 py-2"
+        className={`cursor-pointer bg-white sm:rounded-lg shadow-lg  mb-4 py-2 border-l-8 ${
+          isPickup
+            ? "border-green-500"
+            : isReturn
+            ? "border-blue-500"
+            : "border-gray-500"
+        }`}
       >
-        <div
-          className={`absolute left-0 top-0 h-full ${
-            isPickup ? "bg-green-500" : isReturn ? "bg-blue-500" : "bg-gray-500"
-          }`}
-          style={{ width: "6px" }}
-        ></div>
         <div className="pl-1 text-xs truncate">
           {booking.customerName} - {isPickup && "Pickup "}
           {isReturn && "Return "}
